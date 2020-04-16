84b6b7f2873f65cce94ea9e85e24a34012b9e461
Michelle Wiltshire <michelle@designwasabi.com>
2020-04-16 12:18:13 -0400

    updated alignment

--- src/components/byDay.js
+++ src/components/byDay.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/style.css";
-import SunBkg from "../assets/icons/SunBkg.svg";
+// import SunBkg from "../assets/icons/SunBkg.svg";
+// import SunDance from "../components/SunDance";
 
 let ByDay = ({
   title1 = "Learn More",
@@ -13,14 +14,15 @@ let ByDay = ({
   // alt
 }) => {
   return (
-    <div class="md:flex md:items-center">
-      <div className="flex flex-col h-56 md:h-96 justify-center text-center md:text-left md:mx-24">
+    <div className="items-center">
+      <div className="flex flex-col h-56 md:h-96 justify-center text-center align-center">
         <p className={className1}>{children1 ? children1 : title1}</p>
         <h2 className={className2}>{children2 ? children2 : title2}</h2>
         {/* <img src={src} alt={alt} height="100px" width="100px" /> */}
       </div>
       <div>
-        <SunBkg />
+        {/* <SunBkg /> */}
+        {/* <SunDance /> */}
       </div>
     </div>
   );
