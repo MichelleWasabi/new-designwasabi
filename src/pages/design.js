78159681e8cb086368874322810a3b41b21ccc0a
Michelle Wiltshire <michelle@designwasabi.com>
2020-04-21 19:53:40 -0400

    added portfolio page

--- src/pages/design.js
+++ src/pages/design.js
@@ -7,9 +7,25 @@ function DesignPage() {
   return (
     <Layout>
       <SEO
-        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-        title="About"
+        keywords={[`uxdesign`, `product design`, `portfolio`]}
+        title="Product Design Portfolio"
       />
+      <section>
+        <div className="md:container md:items-center mx-auto">
+          <div className="section-header flex flex-col h-56 md:h-96 justify-center text-center">
+            <h2 className="text-xl md:text-4xl ">Featured Projects</h2>
+            <p className="text-base md:text-xl">my main picks</p>
+          </div>
+          <div className="section-body md:flex justify-center">
+            <div className="project-thumbnail max-w-lg md:rounded overflow-hidden shadow-lg mx-0 my-6 md:mx-6 md:my-6 ">
+              <img src={require("../assets/images/michelle1.jpeg")} alt="" className="w-full" />
+            </div>
+            <div className="project-thumbnail max-w-lg md:rounded overflow-hidden shadow-lg  mx-0 my-6 md:mx-6 md:my-6">
+              <img src={require("../assets/images/michelle1.jpeg")} alt="" className="w-full" />
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
